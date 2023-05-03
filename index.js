27187402252ee1e8bba1be911ad26547294b5325
Andrew <a.oppman@gmail.com>
2023-05-02 20:57:49 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+//let text = newHeader.textContent
+
+
+//let identifyHeader = document.querySelector("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = 'Drew is the champion';
+//document.h1.append(identifyHeader);
+//newHeader.append(text);
